95cfd4771d75a75442fce2d8a419eb0d01d3bdc2
Deodewanto07 <143709679+Deodewanto07@users.noreply.github.com>
2023-12-17 17:39:46 +0700

    Update style.cc

---
+++ style.cc
@@ -0,0 +1,16 @@
+.container-1 {
+    background-color: #5063f0;
+    width: 100%;
+    text-align: center;
+    padding: 10px 25px;
+    color: white;
+}
+
+.tr-vertical-top {
+   vertical-align: text-bottom;
+}
+
+.image-container {
+  padding:20px 20px 50px 20px;
+  background-color:#4d4d4d;
+}
